4d5b230ff60153a60e2942c11f3864e8f1cc4c9e
William Tobin <tobinw2@rpi.edu>
2016-01-13 13:36:31 -0500

    updating petsc inclusion method to use the config file provided by petsc as well and add in the petsc library and include directory missing from that config file

--- cmake/FindPETSc.cmake
+++ cmake/FindPETSc.cmake
@@ -1,63 +1,29 @@
-# todo (l) Bill : everything except petsc and blaslapack is optional
-
 include(util)
 checkSetParam(PETSC_DIR TRUE)
 checkSetParam(PETSC_ARCH TRUE)
 
-find_package(PkgConfig)
-set(ENV{PKG_CONFIG_PATH} "${PETSC_DIR}/${PETSC_ARCH}/lib/pkgconfig/")
-pkg_check_modules(PC_PETSC PETSc)
+find_path(PETSC_CONFIG_DIR PETScConfig.cmake
+          HINTS ${PETSC_DIR}/${PETSC_ARCH}/lib/petsc/conf/)
+
+include(${PETSC_CONFIG_DIR}/PETScConfig.cmake)
 
 find_path(PETSC_INCLUDE_DIR_ONE petscksp.h
-          HINTS ${PC_PETSC_INCLUDEDIR}
+          HINTS ${PETSC_DIR}
           PATH_SUFFIXES include)
 
 find_path(PETSC_INCLUDE_DIR_TWO petscfix.h
-          HINTS ${PC_PETSC_PREFIX}/${PETSC_ARCH}
+          HINTS ${PETSC_DIR}/${PETSC_ARCH}
           PATH_SUFFIXES include)
 
-set(PETSC_INCLUDE_DIRS ${PETSC_INCLUDE_DIR_ONE} ${PETSC_INCLUDE_DIR_TWO})
+set(PETSC_INCLUDE_DIRS ${PETSC_PACKAGE_INCLUDES} ${PETSC_INCLUDE_DIR_ONE} ${PETSC_INCLUDE_DIR_TWO})
+describeVar(PETSC_INCLUDE_DIRS)
 
 find_library(PETSC_LIB petsc
-             HINTS ${PC_PETSC_PREFIX}/${PETSC_ARCH}
+             HINTS ${PETSC_DIR}/${PETSC_ARCH}
              PATH_SUFFIXES lib)
-
-find_library(SUPERLU_LIB superlu_dist_4.1
-             HINTS ${PC_PETSC_PREFIX}/${PETSC_ARCH}
-             PATH_SUFFIXES lib)
-
-find_package(parmetis)
-find_package(zoltan)
-find_package(scotch)
-
-find_library(LAPACK_LIB f2clapack
-             HINTS ${PC_PETSC_PREFIX}/${PETSC_ARCH}
-             PATH_SUFFIXES lib)
-
-if(NOT ${LAPACK_LIB})
-  find_library(LAPACK_LIB flapack
-               HINTS ${PC_PETSC_PREFIX}/${PETSC_ARCH}
-               PATH_SUFFIXES lib)
-endif()
-
-find_library(BLAS_LIB f2cblas
-             HINTS ${PC_PETSC_PREFIX}/${PETSC_ARCH}
-             PATH_SUFFIXES lib)
-
-if(NOT ${BLAS_LIB})
-  find_library(BLAS_LIB fblas
-               HINTS ${PC_PETSC_PREFIX}/${PETSC_ARCH}
-               PATH_SUFFIXES lib)
-endif()
-
 
 set(PETSC_LIBRARIES ${PETSC_LIB} 
-                    ${SUPERLU_LIB} 
-                    ${PARMETIS_LIBRARIES} 
-                    ${ZOLTAN_LIBRARIES}
-		    ${SCOTCH_LIBS}
-                    ${LAPACK_LIB}
-                    ${BLAS_LIB})
+                    ${PETSC_PACKAGE_LIBS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(PETSc DEFAULT_MSG 
